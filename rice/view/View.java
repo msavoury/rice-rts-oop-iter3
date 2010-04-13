f27690d111a17581ec6080c1d8da582678593e7c
ScandinavianBlue <ScandinavianBlue@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-13 19:05:09 +0000

    Initial Classes Created!

---
+++ rice/view/View.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rice.view;
+
+/**
+ *
+ * @author spock
+ */
+public class View {
+
+    public void refresh(){
+    }
+
+}

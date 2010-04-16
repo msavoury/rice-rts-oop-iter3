a6fa71f724a0b68bcbce38f4e3b9f5fd4f941198
leszekkarcz@interia.pl <leszekkarcz@interia.pl@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-16 23:02:44 +0000

    Initial commit of map package

---
+++ trunk/trunk/rice/model/map/AreaMap.java
@@ -0,0 +1,10 @@
+package rice.model.map;
+
+public class AreaMap extends Map {
+	
+	public AreaMap(int[][] terrain)
+	{
+		
+	}
+
+}
---
+++ trunk/trunk/rice/model/map/Map.java
@@ -0,0 +1,61 @@
+package rice.model.map;
+
+import java.awt.Dimension;
+import java.util.List;
+
+public abstract class Map {
+	private Tile[][] tiles;
+	private int width;
+	private int height;
+	private MapPositionTranslator mpt;
+	
+	public Map(Tile[][] tiles, int width, int height)
+	{
+		this.tiles=tiles;
+		this.width=width;
+		this.height=height;
+	}
+	
+	//Return the width of the map
+	public int getWidth()
+	{
+		return this.width;
+	}
+	//Return the width of the map
+	public int getHeight()
+	{
+		return this.height;
+	}
+	
+	//Check if the input location is within map bounds
+	public boolean verifyLocation(int x, int y)
+	{
+		if((x<0) || (x>=this.width) || (y<0) || (y>=this.height))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	//returns a tile at position x,y
+	public Tile getTile(int x, int y)
+	{
+		return this.tiles[y][x];
+	}
+	
+	//return adjacent position depending on current position and angle
+	public Dimension getAdjecentPosition(int x, int y, double angle)
+	{
+		return this.mpt.getAdjecentPosition(x, y, angle);
+	}
+	
+	//return a list of positions surrounding the origin with a certain radius
+	public List<Dimension> getPositionRing(int originX, int originY, int radius)
+	{
+		return this.mpt.getPositionRing(originX, originY, radius);
+	}
+	
+}

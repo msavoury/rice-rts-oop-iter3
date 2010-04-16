2d8ebb71cb82efef3123d1ea31a991fab60e2edd
ScandinavianBlue <ScandinavianBlue@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-16 22:27:04 +0000

    Initial Interfaces for View Created

---
+++ trunk/rice/view/SelfDrawingObject.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rice.view;
+
+/**
+ *
+ * @author spock
+ */
+public class SelfDrawingObject {
+
+}

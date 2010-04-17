0df6037154a66c09a694c00763ab286ac7a686e2
ScandinavianBlue <ScandinavianBlue@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-17 23:40:49 +0000

    added images and fixed arrow locations

--- trunk/rice/view/GraphicsTable.java
+++ trunk/rice/view/GraphicsTable.java
@@ -24,7 +24,24 @@ public class GraphicsTable {
         try{
             graphics.put( "rice", TextureIO.newTexture(ImageIO.read(new File("rice/images/testImage.jpg")), true));
             graphics.put( "iceTest", TextureIO.newTexture(ImageIO.read(new File("rice/images/iceTiletest.jpg")), true));
-            graphics.put( "testArrow", TextureIO.newTexture(ImageIO.read(new File("rice/images/testarrow.png")), true));
+            graphics.put( "arrowRedN", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedN.png")), true));
+            graphics.put( "arrowRedNW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedNW.png")), true));
+            graphics.put( "arrowRedNE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedNE.png")), true));
+            graphics.put( "arrowRedS", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedS.png")), true));
+            graphics.put( "arrowRedSW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedSW.png")), true));
+            graphics.put( "arrowRedSE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowRedSE.png")), true));
+            graphics.put( "arrowBlueN", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueN.png")), true));
+            graphics.put( "arrowBlueNW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueNW.png")), true));
+            graphics.put( "arrowBlueNE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueNE.png")), true));
+            graphics.put( "arrowBlueS", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueS.png")), true));
+            graphics.put( "arrowBlueSW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueSW.png")), true));
+            graphics.put( "arrowBlueSE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowBlueSE.png")), true));
+            graphics.put( "arrowGreenN", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenN.png")), true));
+            graphics.put( "arrowGreenNW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenNW.png")), true));
+            graphics.put( "arrowGreenNE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenNE.png")), true));
+            graphics.put( "arrowGreenS", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenS.png")), true));
+            graphics.put( "arrowGreenSW", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenSW.png")), true));
+            graphics.put( "arrowGreenSE", TextureIO.newTexture(ImageIO.read(new File("rice/images/arrowGreenSE.png")), true));
         }
         catch(Exception d){
             d.printStackTrace();

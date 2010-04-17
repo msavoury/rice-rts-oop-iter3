18724fdd7613551a57682d2395099a068f48025d
marcossavoury <marcossavoury@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-17 02:07:22 +0000

    Adding unit package and empty Unit.java

---
+++ trunk/rice/model/unit/Unit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rice.model.unit;
+
+import rice.model.Controllable;
+
+/**
+ *
+ * @author Marcos
+ */
+public abstract class Unit extends Controllable {
+
+}

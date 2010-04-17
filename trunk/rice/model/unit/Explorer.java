1bd247ed711f748c801c53aecd5fc70cd5f9a196
marcossavoury <marcossavoury@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-17 02:48:01 +0000

    Adding remainder of unit types and unitowner, workerowner interfaces

---
+++ trunk/rice/model/unit/Explorer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rice.model.unit;
+
+import rice.view.MSVisitor;
+import rice.view.UOVisitor;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Explorer extends Unit{
+
+    public void accept(UOVisitor u) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void accept(MSVisitor m) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ trunk/rice/model/unit/Melee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rice.model.unit;
+
+import rice.view.MSVisitor;
+import rice.view.UOVisitor;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Melee extends Unit {
+  public void accept(MSVisitor v){
+
+    }
+  public void accept(UOVisitor u) {
+  //      throw new UnsupportedOperationException("Not supported yet.");
+  }
+}

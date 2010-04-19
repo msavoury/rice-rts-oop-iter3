52f3377cb3cefb38f05976621c2bec2fa4847762
ScandinavianBlue <ScandinavianBlue@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-19 18:30:41 +0000

    added breed

--- trunk/rice/model/ability/BreedAbility.java
+++ trunk/rice/model/ability/BreedAbility.java
@@ -1,19 +1,39 @@
 package rice.model.ability;
 
-import rice.model.Controllable;
+import java.util.Scanner;
+import rice.model.command.Breed;
+import rice.model.structures.Capital;
 
 public class BreedAbility extends Ability
 {
-
-	public BreedAbility(Controllable c) {
+        Capital c;
+	public BreedAbility(Capital c) {
 		super(c);
+                this.c = c;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void acceptInput(String input) {
 		// TODO Auto-generated method stub
-		
+		switch (state){
+			case START:
+			  if(input.equals("CONFIRM_SELECTION_NO_ARGS")){
+				  state = CAPTURING;
+				  setName("Enter Amount");
+			  }
+			  break;
+			case CAPTURING:
+			  
+                              System.out.println(input);
+                              if(input.equals("HOTKEY_2")){
+                                  int nw = 2;
+                                  c.addCommand(new Breed(c, nw));
+                              }
+                              reset();
+			  
+			break;
+		  }
 	}
 
 	@Override

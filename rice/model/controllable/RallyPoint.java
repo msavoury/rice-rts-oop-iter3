a26d4b5b765b9a34aab6af8259fd20a75cc9af12
leszekkarcz@interia.pl <leszekkarcz@interia.pl@47def3bd-7d5a-471c-2c3d-1f21ae07d815>
2010-04-19 20:28:24 +0000

    -base stats methods for rally

--- rice/model/controllable/RallyPoint.java
+++ rice/model/controllable/RallyPoint.java
@@ -1,6 +1,7 @@
 package rice.model.controllable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import rice.model.Controllable;
@@ -14,11 +15,14 @@ import rice.view.ViewableRallyPoint;
 import rice.view.ViewableUnit;
 
 public class RallyPoint extends Controllable implements ViewableRallyPoint, UnitOwner {
-	private ArrayList<Unit> combatArmy = new ArrayList<Unit>();
-	private ArrayList<Unit> supportArmy = new ArrayList<Unit>();
+	private ArrayList<Unit> combatArmy;
+	private ArrayList<Unit> supportArmy;
 	
-	public RallyPoint(String typeName, int id, Player owner) {
+	public RallyPoint(String typeName, int id, Player owner)
+	{
 		super(typeName, id, owner);
+		combatArmy = new ArrayList<Unit>();
+		supportArmy = new ArrayList<Unit>();
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -43,12 +47,27 @@ public class RallyPoint extends Controllable implements ViewableRallyPoint, Unit
 	
 	public List<Unit> getCombatArmy()
 	{
-		return this.combatArmy;
+		if(this.combatArmy!=null) 
+		{
+			return this.combatArmy;
+		}
+		else
+		{
+			return new ArrayList<Unit>();
+		}
+		
 	}
 	
 	public List<Unit> getSupportArmy()
 	{
-		return this.supportArmy;
+		if(this.supportArmy!=null) 
+		{
+			return this.supportArmy;
+		}
+		else
+		{
+			return new ArrayList<Unit>();
+		}
 	}
 
 	public List<ViewableUnit> getAllViewableUnits()
@@ -77,11 +96,106 @@ public class RallyPoint extends Controllable implements ViewableRallyPoint, Unit
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public double getHealth()
+	{
+		int health=0;
+		List<Unit> units = this.getCombatArmy();
+		if(units != null)
+		{
+			Iterator<Unit> iter = units.iterator();
+			while(iter.hasNext())
+			{
+				health+=iter.next().getHealth();
+			}
+		}
+		return health;
+	}
+	
+	public int getAttack()
+	{
+		int attack=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		while(iter.hasNext())
+		{
+			attack+=iter.next().getAttack();
+		}
+		return attack;
+	}
+	
+	public int getDefense()
+	{
+		int defense=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		while(iter.hasNext())
+		{
+			defense+=iter.next().getDefense();
+		}
+		return defense;
+	}
+	
+	public int getArmor()
+	{
+		int armor=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		while(iter.hasNext())
+		{
+			armor+=iter.next().getArmor();
+		}
+		return armor;
+	}
+	
+	public void takeDamage(double value)
+	{
+		  if(value<0)
+		  {
+			  value=value*(-1);
+		  }
+		  double dmg=(double)this.getArmor()-value;
+		  double indivDmg=dmg/this.getCombatArmy().size();
+		  Iterator<Unit> iter = this.getCombatArmy().iterator();
+			while(iter.hasNext())
+			{
+				iter.next().changeHealth(-indivDmg);
+			}
+	}
+	
+	public double getMaxHealth()
+	{
+		int maxHealth=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		while(iter.hasNext())
+		{
+			maxHealth+=iter.next().getMaxHealth();
+		}
+		return maxHealth;
+	}
 
+	public int getVisibilityRadius()
+	{
+		int visibility=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		while(iter.hasNext())
+		{
+			visibility=Math.max(iter.next().getSpeed(), visibility);
+		}
+		return visibility;
+	}
+	
 	@Override
-	public int getSpeed() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getSpeed()
+	{
+		int speed=0;
+		Iterator<Unit> iter = this.getCombatArmy().iterator();
+		if(iter.hasNext())
+		{
+			speed=iter.next().getSpeed();		
+			while(iter.hasNext())
+			{
+				speed=Math.min(iter.next().getSpeed(), speed);
+			}
+		}
+		return speed;
 	}
 
 	@Override
